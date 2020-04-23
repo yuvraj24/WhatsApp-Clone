b58036a2145a390d053d929eaa5c8c8cb9d577e8
Yuvraj Pandey <yuvrajpandey24@gmail.com>
2020-04-23 21:09:14 +0530

    Initial Commit

--- web-client/src/index.js
+++ web-client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; 
-import WebAppNavigator from "../src/modules/qrcode/WebAppNavigator";
+import WebAppNavigator from "./modules/qrcode/WebAppNavigator";
 
 ReactDOM.render(
   <React.StrictMode>
